91f082a6b41de495fa3ecefb87d8515cfe5f3ced
Pedro Jean <pedrojean877@gmail.com>
2025-12-17 14:43:25 -0300

    02

--- src/lib/prisma/client.ts
+++ src/lib/prisma/client.ts
@@ -1,15 +1,14 @@
-import { PrismaClient } from '@prisma/client';
+// prisma.config.ts
+import { defineConfig } from '@prisma/config';
 
-const prismaClientSingleton = () => {
-  return new PrismaClient();
-};
-
-type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>;
-
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClientSingleton | undefined;
-};
-
-export const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
-
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+export default defineConfig({
+  // Informa onde está seu schema
+  schema: 'src/lib/prisma/schema.prisma',
+  
+  // Configura a conexão com o banco (substitui a config do schema.prisma)
+  datasource: {
+    provider: 'postgresql',
+    url: process.env.DATABASE_URL,
+    directUrl: process.env.DIRECT_URL,
+  },
+});
